897bc251b519f13195c1bf4b2d7c80d50b8bb2e9
Murodjon Tokhirov <murodtokh1997@gmail.com>
2024-01-10 09:18:27 +0500

    first commit

---
+++ 1-dars/main.py
@@ -0,0 +1,20 @@
+# n = 998
+
+# summa = 1
+# while n>1:
+#     n-=1
+#     summa*=n
+    
+# print(summa)
+
+
+# def factorial(num):
+#     if num == 0:
+#         return 0
+#     else:
+#         return num + factorial(num-1)
+
+
+
+# print(factorial(998))
+#5-> 1*2*3*4*5
