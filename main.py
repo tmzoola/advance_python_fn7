1382369650d9ccf33e5760a8335e890ba283a345
Murodjon Tokhirov <murodtokh1997@gmail.com>
2024-01-10 09:38:29 +0500

    2-dars Decimal va github darsi kodlari

---
+++ main.py
@@ -0,0 +1,34 @@
+# from decimal import Decimal,getcontext, Context
+# import decimal
+# import math
+
+# a = Decimal(0.3)
+# print(a)
+
+# print(a.quantize(Decimal('0.0'), rounding=decimal.ROUND_UP))
+
+# print(getcontext())
+
+
+# getcontext().traps[decimal.DivisionByZero]=False
+
+# new_context = Context(prec = 8, rounding=decimal.ROUND_UP, )
+# decimal.setcontext(new_context)
+
+
+
+# print(Decimal("Infinity")*(-1))
+
+
+
+# import time
+
+# now = time.time()
+
+# new_context = Context(prec = 28, rounding=decimal.ROUND_UP)
+# decimal.setcontext(new_context)
+
+
+# print(Decimal(0.7) - Decimal(0.1))
+
+
